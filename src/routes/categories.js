0eb02622ff4ba3e3de2cc4e328c17e1c9c448fad
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2018-04-14 22:22:35 +0300

    Add eslint config

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,12 +1,13 @@
 const express = require('express');
+
 const router = express.Router();
 
 router.route('/')
-    .get((req, res) => res.json())
-    .post((req, res) => res.json());
+  .get((req, res) => res.json())
+  .post((req, res) => res.json());
 
 router.route('/:id')
-    .get((req, res) => res.json())
-    .delete((req, res) => res.json());
+  .get((req, res) => res.json())
+  .delete((req, res) => res.json());
 
 module.exports = router;
