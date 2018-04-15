f7f895b91c2110c28dc94a8cf0e36279138ec8ba
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2018-04-15 15:26:47 +0300

    Add Tests CRUD

--- src/routes/tests.js
+++ src/routes/tests.js
@@ -1,13 +1,68 @@
 const express = require('express');
+const Test = require('./../models/test');
 
 const router = express.Router();
 
+// TODO: use midlware to handle errors
+
 router.route('/')
-  .get((req, res) => res.json())
-  .post((req, res) => res.json());
+  .get((req, res) => {
+    Test.find((err, test) => {
+      if (err) {
+        return res.status(400).json(err);
+      }
+
+      return res.json(test);
+    });
+  })
+  .post((req, res) => {
+    const newTest = new Test(req.body);
+
+    newTest.save(err => {
+      if (err) {
+        return res.status(400).json(err);
+      }
+
+      return res.json(newTest);
+    });
+  });
 
 router.route('/:id')
-  .get((req, res) => res.json())
-  .delete((req, res) => res.json());
+  .get((req, res) => {
+    Test.findById(req.params.id, (err, test) => {
+      if (err) {
+        return res.status(400).json(err);
+      }
 
+      return res.json(test);
+    });
+  })
+  .put((req, res) => {
+    Test.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+      (err, test) => {
+        if (err) {
+          return res.status(400).json(err);
+        }
+
+        return res.json(test);
+      },
+    );
+  })
+  .delete((req, res) => {
+    Test.findByIdAndRemove(req.params.id, (err, test) => {
+      if (err) {
+        return res.status(400).json(err);
+      }
+
+      const response = {
+        message: 'Test successfully deleted',
+        id: test._id,
+      };
+
+      return res.json(response);
+    });
+  });
 module.exports = router;
